bad7ae717f2d36974b89796c75cd300e23b81283
Alexander Stepanov <stepalxser@gmail.com>
2020-05-19 12:07:39 +0300

    Added priority queue

--- Lafore/Chapter04/queue.py
+++ Lafore/Chapter04/queue.py
@@ -46,19 +46,71 @@ class Queue:
         return self._elem_counter == self._size
 
 
+class PriorityQueue:
+    def __init__(self, size: int) -> None:
+        self._size = size
+        self._state: List[Optional[int]] = [None for _ in range(size)]
+        self._elem_counter = 0
+
+    def __str__(self) -> str:
+        return str(self._state)
+
+    @property
+    def is_empty(self) -> bool:
+        return not self._elem_counter
+
+    @property
+    def is_full(self) -> bool:
+        return self._elem_counter == self._size
+
+    def insert(self, value) -> None:
+        if self._elem_counter == 0:
+            self._state[self._elem_counter] = value
+            self._elem_counter += 1
+        else:
+            insert_index = 0
+            for index in range(self._elem_counter-1, -1, -1):
+                if value > self._state[index]:
+                    self._state[index+1] = self._state[index]
+                else:
+                    insert_index = index
+                    break
+            self._state[insert_index] = value
+            self._elem_counter += 1
+
+    def remove(self) -> int:
+        result = self._state[self._elem_counter-1]
+        self._state[self._elem_counter-1] = None
+        self._elem_counter -= 1
+        return result
+
+    def peek(self) -> int:
+        return self._state[self._elem_counter]
+
+
 if __name__ == '__main__':
-    queue = Queue(10)
-    for value in range(10, 110, 10):
-        queue.insert(value)
-    print(queue)
-    for _ in range(5):
-        print(queue.remove())
-    print(queue)
+    # queue = Queue(10)
+    # for value in range(10, 110, 10):
+    #     queue.insert(value)
+    # print(queue)
+    # for _ in range(5):
+    #     print(queue.remove())
+    # print(queue)
+    #
+    # for value in range(3):
+    #     queue.insert(value)
+    #
+    # print(queue)
+    # for _ in range(6):
+    #     print(queue.remove())
+    # print(queue)
 
-    for value in range(3):
-        queue.insert(value)
+    pqueue = PriorityQueue(10)
+    for value in range(10):
+        pqueue.insert(value)
+    print(pqueue)
 
-    print(queue)
-    for _ in range(6):
-        print(queue.remove())
-    print(queue)
+    while not pqueue.is_empty:
+        print(pqueue.remove())
+
+    print(pqueue)
