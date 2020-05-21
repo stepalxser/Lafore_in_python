53ec9913e54c55d3be1ca3de82b8440f1eb44562
Alexander Stepanov <stepalxser@gmail.com>
2020-05-21 17:34:21 +0300

    Added SortedList

--- Lafore/Chapter05/linked_list.py
+++ Lafore/Chapter05/linked_list.py
@@ -93,29 +93,89 @@ class TwoWayLinkedList:
         return True if self.first is None else False
 
 
+class SortedList:
+    def __init__(self, first=None):
+        self.first: Optional[Link] = first
+
+    def __str__(self) -> str:
+        display_array, current = [], self.first
+        while current is not None:
+            display_array.append(current)
+            current = current.next
+        return ' '.join(str(item) for item in display_array)
+
+    def insert(self, link_data: Any) -> None:
+        new_link = Link(link_data)
+        previous = None
+        current = self.first
+
+        while current is not None and link_data > current.data:
+            previous = current
+            current = current.next
+        if previous is None:
+            self.first = new_link
+        else:
+            previous.next = new_link
+        new_link.next = current
+
+    def find(self, key: Any) -> Optional[Link]:
+        current = self.first
+        while current is not None:
+            if current.data == key:
+                return current
+            current = current.next
+        return None
+
+    def delete(self, key: Any) -> Optional[Link]:
+        current = self.first
+        previous = self.first
+        while current is not None:
+            if current.data == key:
+                previous.next = current.next
+                return current
+            previous = current
+            current = current.next
+        return None
+
+    @property
+    def is_empty(self):
+        return True if self.first is None else False
+
+
 if __name__ == '__main__':
-    linked_list = LinkedList()
-    for value in range(10, 60, 10):
-        linked_list.insert_first(value)
-    print(linked_list)
+    # linked_list = LinkedList()
+    # for value in range(10, 60, 10):
+    #     linked_list.insert_first(value)
+    # print(linked_list)
+    #
+    # linked_list.delete_first()
+    # print(linked_list)
+    #
+    # print(linked_list.find(20))
+    #
+    # linked_list.delete(20)
+    # print(linked_list)
+    # print()
+    #
+    #
+    # linked_list = TwoWayLinkedList()
+    # for value in range(10, 60, 10):
+    #     linked_list.insert_last(value)
+    # print(linked_list)
+    # for value in range(10, 60, 10):
+    #     linked_list.insert_first(value)
+    # print(linked_list)
+    #
+    # for _ in range(5):
+    #     print(linked_list.delete_fisrt())
+    # print(linked_list)
 
-    linked_list.delete_first()
-    print(linked_list)
 
-    print(linked_list.find(20))
+    sorted_list = SortedList()
+    for value in range(10, 110, 10):
+        sorted_list.insert(value)
+    print(sorted_list)
 
-    linked_list.delete(20)
-    print(linked_list)
-    print()
-
-    linked_list = TwoWayLinkedList()
-    for value in range(10, 60, 10):
-        linked_list.insert_last(value)
-    print(linked_list)
-    for value in range(10, 60, 10):
-        linked_list.insert_first(value)
-    print(linked_list)
-
-    for _ in range(5):
-        print(linked_list.delete_fisrt())
-    print(linked_list)
+    for value in range(5, 105, 10):
+        sorted_list.insert(value)
+    print(sorted_list)
