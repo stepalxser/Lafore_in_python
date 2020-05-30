a56a85b46bc80f1d05a4673d71b671639825ac12
Alexander Stepanov <stepalxser@gmail.com>
2020-05-30 11:04:02 +0300

    Added 7.3 7.4 programming projects

--- Lafore/Chapter07/projects.py
+++ Lafore/Chapter07/projects.py
@@ -20,35 +20,67 @@ def find_median(array: List[int]) -> int:
                 array[left_index], array[right_index] = array[right_index], array[left_index]
                 left_index += 1
                 right_index -= 1
-        array[swap_index], array[left_index] = array[left_index], array[swap_index]
-
-        return right_index + 1
+        array[left_index], array[swap_index] = array[swap_index], array[left_index]
+        return left_index
 
     def median(left_index, right_index) -> int:
-        if right_index - left_index <= 1:
-            return array[right_index]
         index = partition(left_index, right_index)
         if index == median_index:
             return array[index]
-        elif index > median_index:
-            median(left_border, index-1)
-        else:
-            median(index+1, right_border)
+        if index > median_index:
+            return median(left_border, index-1)
+        if index < median_index:
+            return median(index+1, right_border)
+
+    return median(left_border, right_border)
+
+
+# 7.4 programming project
+def find_index(array: List[int], search_index) -> int:
+    left_border = 0
+    right_border = len(array) - 1
+
+    def partition(left_index: int, right_index: int) -> int:
+        swap_index, right_index = right_index, right_index - 1
+        pivot = array[swap_index]
+        while left_index <= right_index:
+            while left_index < right_border and array[left_index] < pivot:
+                left_index += 1
+            while right_index > left_border and array[right_index] > pivot:
+                right_index -= 1
+            if left_index <= right_index:
+                array[left_index], array[right_index] = array[right_index], array[left_index]
+                left_index += 1
+                right_index -= 1
+        array[left_index], array[swap_index] = array[swap_index], array[left_index]
+        return left_index
+
+    def median(left_index, right_index) -> int:
+        index = partition(left_index, right_index)
+        if index == search_index:
+            return array[index]
+        if index > search_index:
+            return median(left_border, index - 1)
+        if index < search_index:
+            return median(index + 1, right_border)
 
     return median(left_border, right_border)
 
 
 if __name__ == '__main__':
-    data = choices(range(100), k=7)
+    data = choices(range(100), k=11)
     print(data)
     print(sorted(data))
-    print(sorted(data)[3])
+    print(sorted(data)[5])
     print(find_median(data))
     print(data)
     print('end')
 
-
-
-
-
-
+    data = choices(range(100), k=11)
+    print(data)
+    print(sorted(data))
+    index = 7
+    print(sorted(data)[index])
+    print(find_index(data, index))
+    print(data)
+    print('end')
