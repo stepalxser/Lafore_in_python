9e09136be66b2495c8470ac5e541a58ede494856
Alexander Stepanov <stepalxser@gmail.com>
2020-06-02 13:35:57 +0300

    Added binary tree implementation

--- Lafore/Chapter08/binary_tree.py
+++ Lafore/Chapter08/binary_tree.py
@@ -11,7 +11,7 @@ class Node:
         self.right = right
 
     def __str__(self) -> str:
-        return f'{self.value:>4} {self.data:>4}'
+        return str(self.value)
 
 
 class Tree:
@@ -106,7 +106,73 @@ class Tree:
         elif current.left is None or current.right is None:
             return self._delete_one(parent, current)
         else:
-            return self._delete_two(parent,current)
+            return self._delete_two(parent, current)
+
+    def _pre_order(self, local_root: Node) -> None:
+        if local_root is not None:
+            print(local_root.value, end=' ')
+            self._pre_order(local_root.left)
+            self._pre_order(local_root.right)
+
+    def _in_order(self, local_root: Node) -> None:
+        if local_root is not None:
+            self._in_order(local_root.left)
+            print(local_root.value, end=' ')
+            self._in_order(local_root.right)
+
+    def _post_order(self, local_root: Node) -> None:
+        if local_root is not None:
+            self._post_order(local_root.left)
+            self._post_order(local_root.right)
+            print(local_root.value, end=' ')
+
+    def traverse(self, type: str) -> None:
+        commands = {'pre': self._pre_order, 'in': self._in_order, 'post': self._post_order}
+        commands[type](self._root)
+        print()
+
+    def display(self) -> None:
+        global_stack,  blanks, is_row_empty = [self._root], 64, False
+        print(64 * '*')
+
+        while not is_row_empty:
+            local_stack, is_row_empty = [], True
+            while len(global_stack) != 0:
+                temp = global_stack.pop()
+                if temp is not None:
+                    print(f'{temp.value:^{blanks}}', end='')
+                    local_stack.extend((temp.left, temp.right))
+                    is_row_empty = False if temp.left is not None or temp.right is not None else is_row_empty
+                else:
+                    print(f'{"--":^{blanks}}', end='')
+                    local_stack.extend((None, None))
+            print()
+            blanks //= 2
+            while len(local_stack) != 0:
+                global_stack.append(local_stack.pop())
+        print(64 * '*')
 
 
-
+if __name__ == '__main__':
+    tree = Tree()
+    tree.insert(50)
+    tree.insert(25)
+    tree.insert(75)
+    tree.insert(12)
+    tree.insert(37)
+    tree.insert(43)
+    tree.insert(30)
+    tree.insert(33)
+    tree.insert(87)
+    tree.insert(93)
+    tree.insert(97)
+    tree.traverse('in')
+    tree.traverse('pre')
+    tree.traverse('post')
+    tree.display()
+    tree.delete(33)
+    tree.display()
+    tree.delete(93)
+    tree.display()
+    tree.delete(25)
+    tree.display()
