62b39f57b8204b54b71dd17410c65cd892f0b88e
Alexander Stepanov <stepalxser@gmail.com>
2020-06-12 18:33:37 +0300

    Added 10.1 10.2 10.3 programming projects

--- Lafore/Chapter10/tree234.py
+++ Lafore/Chapter10/tree234.py
@@ -15,7 +15,7 @@ class Node:
 
     def __str__(self) -> str:
         result = '|'.join(f'{item:>2}' if item is not None else '  ' for item in self.data_array)
-        return ''.join(('|', result, '|'))
+        return ''.join(('<', result, '>'))
 
     @property
     def if_leaf(self) -> bool:
@@ -73,6 +73,7 @@ class Tree234:
             if current_node.is_full:
                 self.split(current_node)
                 current_node = current_node.parent
+                current_node = self.get_next_child(current_node, value)
             elif current_node.if_leaf:
                 break
             else:
@@ -86,27 +87,27 @@ class Tree234:
         return current_node.child_array[len(current_node)]
 
     def split(self, current_node):
-        item_c = current_node.remove_item()
-        item_b = current_node.remove_item()
-        child_2 = current_node.disconnect_child(2)
-        child_3 = current_node.disconnect_child(3)
         new_right_node = Node()
+        new_right_node.insert_item(current_node.remove_item())
+        new_right_node.connect_child(0, current_node.disconnect_child(2))
+        new_right_node.connect_child(1, current_node.disconnect_child(3))
+
+        item_b = current_node.remove_item()
 
         if current_node is self._root:
-            root = Node()
-            parent = root
-            root.connect_child(0, current_node)
+            self._root = Node()
+            parent = self._root
+            self._root.connect_child(0, current_node)
         else:
             parent = current_node.parent
+
         parent_index = parent.insert_item(item_b)
 
-        for index in range(len(parent), parent_index-1, -1):
-            parent.connect_child(index, parent.disconnect_child(index-1))
-        parent.connect_child(parent_index, new_right_node)
+        for child_index in range(len(parent)-1, parent_index, -1):
+            temp = parent.disconnect_child(child_index)
+            parent.connect_child(child_index+1, temp)
 
-        new_right_node.insert_item(item_c)
-        new_right_node.connect_child(0, child_2)
-        new_right_node.connect_child(1, child_3)
+        parent.connect_child(parent_index+1, new_right_node)
 
     def display(self) -> None:
         global_stack, blanks, is_row_empty = [self._root], 48, False
@@ -125,13 +126,52 @@ class Tree234:
             print()
         print(48 * '*')
 
+    # programming project 10.1
+    def min(self) -> int:
+        current_node = self._root
+        while not current_node.if_leaf:
+            current_node = current_node.child_array[0]
+        return current_node.data_array[0]
+
+    # programming project 10.2
+    def in_order_traverse(self, node=None):
+        node = self._root if node is None else node
+        for index in range(4):
+            if node.child_array[index] is not None:
+                self.in_order_traverse(node=node.child_array[index])
+            if index != 3 and node.data_array[index] is not None:
+                print(node.data_array[index], end=' ')
+
+    # programming project 10.3
+    @classmethod
+    def sorted(cls, data: List) -> List:
+        temp = cls()
+        for item in data:
+            temp.insert(item)
+
+        def array_traverse(node=None, data=None) -> List:
+            data = [] if data is None else data
+            node = temp._root if node is None else node
+            for index in range(4):
+                if node.child_array[index] is not None:
+                    array_traverse(node=node.child_array[index], data=data)
+                if index != 3 and node.data_array[index] is not None:
+                    data.append(node.data_array[index])
+            return data
+        return array_traverse(node=temp._root)
+
 
 if __name__ == '__main__':
-   tree = Tree234()
-   tree.insert(70)
-   tree.insert(50)
-   tree.display()
-   tree.insert(30)
-   tree.display()
-   tree.insert(40)
-   tree.display()
+    tree = Tree234()
+
+    for index in range(10, 100, 10):
+        tree.insert(index)
+        tree.display()
+
+    print(tree.min())
+    tree.in_order_traverse()
+    print()
+
+    array = [item for item in range(10, 0, -1)]
+    array_2 = Tree234.sorted(array)
+    print(array_2)
